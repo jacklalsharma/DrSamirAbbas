f2be99aa6669dac73189bec43e409ef459611a88
Adarsh <jacklalsharma12@gmail.com>
2018-04-09 23:39:57 +0530

    All UI implematation almost complete.

---
+++ app/src/main/java/com/dr/SamirAbbas/adapters/DoctorInfoAdapter.java
@@ -0,0 +1,8 @@
+package com.dr.SamirAbbas.adapters;
+
+/**
+ * Created by Anurag on 4/9/2018.
+ */
+
+public class DoctorInfoAdapter {
+}
---
+++ app/src/main/java/com/dr/SamirAbbas/models/DoctorInfo.java
@@ -0,0 +1,8 @@
+package com.dr.SamirAbbas.models;
+
+/**
+ * Created by Anurag on 4/9/2018.
+ */
+
+public class DoctorInfo {
+}
