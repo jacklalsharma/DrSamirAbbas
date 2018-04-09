816db402e6b75523f921d3bf37023248be5c683e
Adarsh <jacklalsharma12@gmail.com>
2018-04-09 19:30:45 +0530

    Changes to the completed UI made.

--- app/src/main/java/com/dr/SamirAbbas/activities/NavigationActivity.java
+++ app/src/main/java/com/dr/SamirAbbas/activities/NavigationActivity.java
@@ -6,19 +6,14 @@ import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.LinearLayout;
 
 import com.dr.SamirAbbas.R;
 
-public class NavigationActivity extends BaseActivity
-        implements NavigationView.OnNavigationItemSelectedListener, View.OnClickListener{
-
-
+public class NavigationActivity extends BaseActivity implements NavigationView.OnNavigationItemSelectedListener, View.OnClickListener{
 
 
     @Override
@@ -84,21 +79,7 @@ public class NavigationActivity extends BaseActivity
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
-        int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
-        }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
--- app/src/main/java/com/dr/SamirAbbas/activities/SaveBookingActivity.java
+++ app/src/main/java/com/dr/SamirAbbas/activities/SaveBookingActivity.java
@@ -1,7 +1,5 @@
 package com.dr.SamirAbbas.activities;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
